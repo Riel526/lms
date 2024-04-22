1f80b15eaaafa0312383d0e0ebc4572f3199f420
Riel526 <gabpadolina05@gmail.com>
2024-04-23 03:27:36 +0800

    frontend router initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>NEW</h1>
+      <Router>
+        <Routes>
+          <Route path = '/' element ={<Login/>}/>
+          <Route path = '/signup' element={<Signup/>}/>
+          <Route path = '/home' element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
