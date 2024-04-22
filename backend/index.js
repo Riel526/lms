5cfe03b000b3664df4f95919e148467c0dc5038c
Riel526 <gabpadolina05@gmail.com>
2024-04-23 01:24:21 +0800

    express connection to database initialization

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import {config} from "dotenv"
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+config()
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('error', (error)=>{
+    console.error
+})
+
+db.once('open', ()=>{
+    console.log("connected to database")
+})
